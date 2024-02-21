2828f20486afd3110d5bb562370eea07aa16e8a5
Charan Polisetty <charan.ssc006@gmail.com>
2024-02-22 00:31:57 +0530

    added logout button

--- lib/profile/pages/ProfilePage.dart
+++ lib/profile/pages/ProfilePage.dart
@@ -1,12 +1,14 @@
 import 'package:balencebeats/Emergencycontacts/pages/em_contact.dart';
+import 'package:balencebeats/Screens/Login_Page.dart';
 import 'package:balencebeats/profile/components/privacypolicy.dart';
 import 'package:balencebeats/profile/components/terms.dart';
 import 'package:balencebeats/profile/components/text_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key, required this.username});
+  const ProfilePage({Key? key, required this.username}) : super(key: key);
   final String username;
 
   @override
@@ -14,62 +16,76 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _signOut() async {
+    try {
+      await _auth.signOut();
+      Get.offAll(LoginPage());
+    } catch (e) {
+      print("Error signing out: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 125.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundImage: const AssetImage('assets/charan.jpg'),
-                    radius: Get.height / 16,
-                  ),
-                  SizedBox(
-                    width: Get.width / 8,
-                  ),
-                  Text(
-                    widget.username,
-                    style: const TextStyle(color: Colors.black, fontSize: 30),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: Get.height/12,),
-              const TextBar(
-                  text: 'Account', icon: Icons.navigate_next_outlined),
-              GestureDetector(
-                onTap: () => {
-                  Get.to(EmergengyContact(), transition: Transition.rightToLeft)
-                },
-                child: const TextBar(
-                    text: 'Emergency Contacts',
-                    icon: Icons.navigate_next_outlined),
-              ),
-              const TextBar(
-                  text: 'Language', icon: Icons.navigate_next_outlined),
-              const TextBar(
-                  text: 'Whats new', icon: Icons.navigate_next_outlined),
-              const TextBar(text: 'FAQ', icon: Icons.navigate_next_outlined),
-              GestureDetector(
-                onTap: () => {Get.to(MyTerms(), transition: Transition.rightToLeft)},
-                child: const TextBar(
-                    text: 'Terms Of Service ',
-                    icon: Icons.navigate_next_outlined),
-              ),
-              GestureDetector(
-                onTap: () => {
-                  Get.to(MyPrivacyPolicy(), transition: Transition.rightToLeft)
-                },
-                child: const TextBar(
-                    text: 'Privacy Policy', icon: Icons.navigate_next_outlined),
-              ),
-              SizedBox(height: Get.height / 4),
-            ],
-          ),
+      body: Padding(
+        padding:
+            const EdgeInsets.symmetric(horizontal: 20.0, vertical: 125.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage: const AssetImage('assets/charan.jpg'),
+                  radius: Get.height / 16,
+                ),
+                SizedBox(
+                  width: Get.width / 8,
+                ),
+                Text(
+                  widget.username,
+                  style: const TextStyle(color: Colors.black, fontSize: 30),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: Get.height / 12,
+            ),
+            const TextBar(
+                text: 'Account', icon: Icons.navigate_next_outlined),
+            GestureDetector(
+              onTap: () => Get.to(EmergengyContact(),
+                  transition: Transition.rightToLeft),
+              child: const TextBar(
+                  text: 'Emergency Contacts',
+                  icon: Icons.navigate_next_outlined),
+            ),
+            const TextBar(
+                text: 'Language', icon: Icons.navigate_next_outlined),
+            const TextBar(
+                text: 'Whats new', icon: Icons.navigate_next_outlined),
+            const TextBar(text: 'FAQ', icon: Icons.navigate_next_outlined),
+            GestureDetector(
+              onTap: () =>
+                  Get.to(MyTerms(), transition: Transition.rightToLeft),
+              child: const TextBar(
+                  text: 'Terms Of Service ',
+                  icon: Icons.navigate_next_outlined),
+            ),
+            GestureDetector(
+              onTap: () => Get.to(MyPrivacyPolicy(),
+                  transition: Transition.rightToLeft),
+              child: const TextBar(
+                  text: 'Privacy Policy', icon: Icons.navigate_next_outlined),
+            ),
+            SizedBox(height: Get.height / 4),
+            ElevatedButton(
+              onPressed: _signOut,
+              child: Text('Logout'),
+            ),
+          ],
         ),
       ),
     );
